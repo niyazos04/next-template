6ee0923de00310558a74b77eb05de568b84ec857
Niyaz <niyaz@incrediblevisibility.com>
2025-03-05 16:37:32 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone", // Ensure compatibility with AWS Amplify
+};
 
 export default nextConfig;
